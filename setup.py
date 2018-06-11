f4cce6a96ee510934a7b61203ec24f9969965fdd
Mohab Usama <mohab.abdelhameed@zalando.de>
2018-06-11 13:53:48 +0200

    Adjust setup file and tox

--- setup.py
+++ setup.py
@@ -24,39 +24,40 @@ def load_req(req):
 TEST_REQUIREMENTS = load_req('test_requirements.txt')
 
 
-setup(
-    name='opentracing-utils',
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=open('README.rst').read(),
-    license=open('LICENSE').read(),
-    packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'future',
-        'opentracing',
-    ],
-    setup_requires=[
-        'opentracing',
-        'pytest-runner',
-    ],
-    test_suite='tests',
-    tests_require=TEST_REQUIREMENTS,
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Environment :: Console',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
-        'Topic :: Utilities',
-        'Topic :: System :: Monitoring',
-        'Topic :: System :: Networking :: Monitoring',
-    ]
-)
+if __name__ == '__main__':
+    setup(
+        name='opentracing-utils',
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=open('README.rst').read(),
+        license=open('LICENSE').read(),
+        packages=find_packages(exclude=['tests']),
+        install_requires=[
+            'future',
+            'opentracing',
+        ],
+        setup_requires=[
+            'opentracing',
+            'pytest-runner',
+        ],
+        test_suite='tests',
+        tests_require=TEST_REQUIREMENTS,
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Environment :: Console',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: MacOS :: MacOS X',
+            'Topic :: Utilities',
+            'Topic :: System :: Monitoring',
+            'Topic :: System :: Networking :: Monitoring',
+        ]
+    )
