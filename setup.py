224c24d7a16fa6fb7c579da62b1fc04465a19ba5
Mohab Usama <mohab.usama@gmail.com>
2019-01-16 14:05:28 +0100

    Return tracer from init (#28)
    * Return tracer from init
    
    Signed-off-by: Mohab Usama <mohab.abdelhameed@zalando.de>
    
    * Version 0.17
    
    Signed-off-by: Mohab Usama <mohab.abdelhameed@zalando.de>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 PY3 = sys.version_info.major == 3
 
 MAIN_PACKAGE = 'opentracing_utils'
-VERSION = '0.16'
+VERSION = '0.17'
 DESCRIPTION = 'OpenTracing utilities library'
 
 
