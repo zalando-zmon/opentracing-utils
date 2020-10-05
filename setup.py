1894beefcff5d11959417b129bb1b4c9894091a3
Hanno Hecker <hanno@zalando.de>
2020-10-05 13:58:53 +0200

    version 0.19
    new version 0.19

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 PY3 = sys.version_info.major == 3
 
 MAIN_PACKAGE = 'opentracing_utils'
-VERSION = '0.18.1'
+VERSION = '0.19'
 DESCRIPTION = 'OpenTracing utilities library'
 
 
